4e0ef3952fdff0c2d581bc1783a4521348d66b94
nligon <nathanaelligon@gmail.com>
2016-08-12 20:51:20 -0700

    Implement initial render and update video information.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
+var App = () => {
+ return (
+    <div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer/>
+      </div>
+      <div className="col-md-5">
+        <VideoList data={window.exampleVideoData}/>
+      </div>
     </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+  );
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
